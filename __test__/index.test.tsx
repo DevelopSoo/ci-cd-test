b04f13421f42777e1be66079a980310424bf289e
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-03-19 16:35:52 +0900

    test

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -5,7 +5,7 @@ describe("Home", () => {
 	it("renders a heading", () => {
 		render(<Home />);
 
-		const heading = screen.getByRole("heading", { name: "Home" });
+		const heading = screen.getByRole("heading", { name: "Home2" });
 
 		expect(heading).toBeInTheDocument();
 	});
